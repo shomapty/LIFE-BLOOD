0d037c50efed95dea5157947caaff10bbd079828
Shakil Mahmud <69shakilmahmud@gmail.com>
2025-01-29 17:21:15 +0600

    Working

--- lib/theme/app_decoration.dart
+++ lib/theme/app_decoration.dart
@@ -3,7 +3,6 @@ import 'package:smuctblooddonation/core/utils/size_utils.dart';
 import 'package:smuctblooddonation/theme/theme_helper.dart';
 
 class AppDecoration {
-  // Fill decorations
   static BoxDecoration get fillGray => BoxDecoration(
         color: appTheme.gray400,
       );
@@ -16,7 +15,6 @@ class AppDecoration {
 }
 
 class BorderRadiusStyle {
-  // Rounded borders
   static BorderRadius get roundedBorder26 => BorderRadius.circular(
         26.h,
       );
